2f34a8ea084dcbfa1219781de5b97ab8da9ff814
Parakoopa <parakoopa@live.de>
2019-04-17 01:36:40 +0200

    Import integration tests directly to be run via package name

---
+++ riptide/tests/integration/__init__.py
@@ -0,0 +1,6 @@
+from .config_test import *
+from .engine_cmd_test import *
+from .engine_exec_test import *
+from .engine_service_test import *
+from .engine_start_stop_test import *
+from .engine_util_test import *
