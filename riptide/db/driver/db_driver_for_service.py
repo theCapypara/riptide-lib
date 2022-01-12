f352125be80d70f6af98b5fe8ba3a004e2282969
Marco Köpcke <marco.koepcke@tudock.de>
2022-01-12 09:08:47 +0100

    Fix db commands not working anymore due to changed signature of db_driver_for_service

--- riptide/db/driver/db_driver_for_service.py
+++ riptide/db/driver/db_driver_for_service.py
@@ -1,5 +1,5 @@
 """Module to resolve database drivers for services"""
-from typing import Union, TYPE_CHECKING
+from typing import Union, TYPE_CHECKING, Optional
 
 import pkg_resources
 
@@ -11,9 +11,11 @@ if TYPE_CHECKING:
 DB_DRIVER_ENTRYPOINT_KEY = 'riptide.db_driver'
 
 
-def get(service_data: dict, service: 'Service') -> Union[AbstractDbDriver, None]:
+def get(service_data: Union['Service', dict], service: Optional['Service'] = None) -> Union[AbstractDbDriver, None]:
     """Returns the db driver instance for this service, if a driver is defined."""
     # Look up package entrypoints for db drivers
+    if service is None:
+        service = service_data
     drivers = {
         entry_point.name:
             entry_point.load() for entry_point in pkg_resources.iter_entry_points(DB_DRIVER_ENTRYPOINT_KEY)
